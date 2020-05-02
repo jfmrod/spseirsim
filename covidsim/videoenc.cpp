9af47d1dc713efd3a608b9db699e3771f9060fd3
jfmrod <jfmrod@d80f574c-7d1c-0410-bcfe-df0529ef636d>
2020-05-02 19:37:06 +0000

    Have added multithreading in previous commit. Adding shapefile reading of dbf information

--- covidsim/videoenc.cpp
+++ covidsim/videoenc.cpp
@@ -12,8 +12,10 @@ extern "C" {
 
 
 AVFrame *videoFrame = nullptr;
+AVFrame *audioFrame = nullptr;
 AVCodecContext *cctx = nullptr;
 SwsContext *swsCtx = nullptr;
+AVCodecContext *acctx = nullptr;
 int frameCounter = 0;
 AVFormatContext *ofctx = nullptr;
 AVOutputFormat *oformat = nullptr;
@@ -22,6 +24,48 @@ int fps = 5;
 //int width = 1920;
 //int height = 1080;
 //int bitrate = 2000;
+
+void audioPushFrame(uint8_t *data){
+  int err;
+  if (!audioFrame) {
+    audioFrame = av_frame_alloc();
+    audioFrame->format = AV_SAMPLE_FMT_U8;
+//    audioFrame->nChannels = 1;
+//    audioFrame->nSamplesPerSec = 22000;
+    
+//    videoFrame->width = cctx->width;
+//    videoFrame->height = cctx->height;
+    if ((err = av_frame_get_buffer(audioFrame, 32)) < 0) {
+      std::cout <<  "Failed to allocate picture" << err << std::endl;
+      return;
+    }
+  }
+
+//  sws_scale(swsCtx, (const uint8_t * const *)&data, inLinesize, 0, cctx->height, videoFrame->data, videoFrame->linesize);
+
+  audioFrame->pts = (1.0/fps)*90000.0*(frameCounter-1);
+//  std::cout << videoFrame->pts <<" " << cctx->time_base.num << " " << cctx->time_base.den << " " << frameCounter<< std::endl;
+  if ((err = avcodec_send_frame(acctx, audioFrame)) < 0) {
+    std::cout << "Failed to send audio frame" << err <<std::endl;
+    return;
+  }
+
+  AVPacket pkt;
+  av_init_packet(&pkt);
+  pkt.data = NULL;
+  pkt.size = 0;
+  pkt.stream_index = 0;
+  pkt.flags |= AV_PKT_FLAG_KEY;
+  if (avcodec_receive_packet(acctx, &pkt) == 0) {
+    static int counter = 0;
+//    std::cout << "pkt key: " << (pkt.flags & AV_PKT_FLAG_KEY) <<" " << pkt.size << " " << (counter++) << std::endl;
+    uint8_t *size = ((uint8_t*)pkt.data);
+//    std::cout << "first: " << (int)size[0] << " " << (int)size[1] << " " << (int)size[2] << " " << (int)size[3] <<" "  << (int)size[4] << " " << (int)size[5] << " " << (int)size[6] << " " << (int)size[7] << std::endl;
+    av_interleaved_write_frame(ofctx, &pkt);
+    av_packet_unref(&pkt);
+  }
+
+}
 
 void videoPushFrame(uint8_t *data){
   int err;
@@ -153,11 +197,18 @@ int videoOpen(int width,int height,int _fps,int bitrate)
   stream->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
   stream->codecpar->width = width;
   stream->codecpar->height = height;
+//  stream->codecpar->aspect_ratio = {width,height};
   stream->codecpar->format = AV_PIX_FMT_YUV420P;
   stream->codecpar->bit_rate = bitrate * 1000;
   avcodec_parameters_to_context(cctx, stream->codecpar);
 //  cctx->time_base = (AVRational){ 1, 1 };
 //  cctx->time_base = (AVRational){ 1, fps };
+
+  cctx->width = width;
+  cctx->height = height;
+//  cctx->aspect_ratio = {width,height};
+  cctx->pix_fmt = AV_PIX_FMT_YUV420P;
+
 
   cctx->pix_fmt = AV_PIX_FMT_YUV420P;
   cctx->time_base = (AVRational){ 1000, 1 };
@@ -208,6 +259,51 @@ int videoOpen(int width,int height,int _fps,int bitrate)
     std::cout << "Failed to open codec" << err << std::endl;
     return -1;
   }
+
+
+/*
+  // audio codec
+
+  AVCodec *acodec = avcodec_find_encoder(oformat->audio_codec);
+  if (!acodec) {
+    std::cout << "can't create audio codec" << std::endl;
+    return -1;
+  }
+
+  AVStream *astream = avformat_new_stream(ofctx, acodec);
+  if (!stream) {
+    std::cout << "can't find audio format" << std::endl;
+    return -1;
+  }
+
+  acctx = avcodec_alloc_context3(acodec);
+  if (!acctx)  {
+    std::cout << "can't create audio codec context" << std::endl;
+    return -1;
+  }
+
+  astream->codecpar->codec_id = oformat->audio_codec;
+  astream->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+  avcodec_parameters_to_context(acctx, astream->codecpar);
+
+  acctx->time_base = (AVRational){ 1000, 1 };
+  acctx->framerate= (AVRational){fps, 1};
+
+
+  acctx->sample_rate = 22050;
+  acctx->channel_layout = 1;
+  acctx->channels = av_get_channel_layout_nb_channels(acctx->channel_layout);
+  // take first format from list of supported formats
+//  acctx->sample_fmt = acodec->sample_fmts[0];
+  acctx->sample_fmt = AV_SAMPLE_FMT_U8;
+  acctx->time_base = (AVRational){1, acctx->sample_rate};
+
+  avcodec_parameters_from_context(astream->codecpar, acctx);
+  if ((err = avcodec_open2(acctx, acodec, NULL)) < 0) {
+    std::cout << "Failed to open audio codec" << err << std::endl;
+    return -1;
+  }
+*/
 
   if (!(oformat->flags & AVFMT_NOFILE)) {
     if ((err = avio_open(&ofctx->pb, "test.mp4", AVIO_FLAG_WRITE)) < 0) {
