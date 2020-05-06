21aaaa4ee8ad96a9812cc996aac7c763ab76f651
João F. Matias Rodrigues <5286814+jfmrod@users.noreply.github.com>
2020-05-06 17:58:02 +0000

    Added state to individuals, changed trigger to use new ICU cases

--- data/make-agegroups.R
+++ data/make-agegroups.R
@@ -4,4 +4,9 @@ write.table(popdem[popdem$name=="Portugal",],file="portugal.agegroups",sep="\t",
 write.table(popdem[popdem$name=="Switzerland",],file="switzerland.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
 write.table(popdem[popdem$name=="Poland",],file="poland.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
 write.table(popdem[popdem$name=="United Kingdom",],file="uk.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
+write.table(popdem[popdem$name=="Slovenia",],file="slovenia.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
+write.table(popdem[popdem$name=="Sweden",],file="sweden.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
+write.table(popdem[popdem$name=="Spain",],file="spain.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
+write.table(popdem[popdem$name=="Germany",],file="germany.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
+write.table(popdem[popdem$name=="France",],file="france.agegroups",sep="\t",col.names=TRUE,row.names=FALSE)
 
