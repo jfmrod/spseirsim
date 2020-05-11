0198f9313e1893f2e06d04c74971d918d6d7cac9
João F. Matias Rodrigues <5286814+jfmrod@users.noreply.github.com>
2020-05-11 09:29:01 +0000

    Added isolation triggers for all and older group. Made video generation optional, tested most european countries

--- videoenc.h
+++ videoenc.h
@@ -1,8 +1,10 @@
 #ifndef VIDEOENC_H
 #define VIDEOENC_H
 
+#include <eutils/estr.h>
+
 //int videoOpen();
-int videoOpen(int width,int height,int fps=30,int bitrate=2000);
+int videoOpen(const estr& fname,int width,int height,int fps=30,int bitrate=2000);
 void videoClose();
 void videoPushFrame(uint8_t *data);
 
